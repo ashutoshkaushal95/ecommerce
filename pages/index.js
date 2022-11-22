e34b86c5d09b7e624d022b023b43fe6cfe39d0c7
ashutoshkaushal95 <ashutoshkaushal2@gmail.com>
2022-11-22 16:01:51 +0530

    Setup backend with sanity database

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ import { Product, HeroBanner, FooterBanner } from '../components';
 const Home = ({ products, bannerData }) => {
   return (
     <>
-      <HeroBanner />
-
+      <HeroBanner heroBanner = {bannerData.length && bannerData[0]} />
+      {console.log(bannerData)}
       <div className='products-heading'>
         <h2>Best Selling Headphones</h2>
         <p>Speakers of many variations</p>
