9c72e61e1804c619591fb3fd41b600024467ff00
ashutoshkaushal95 <ashutoshkaushal2@gmail.com>
2022-11-22 12:38:11 +0530

    Added Components for Homepage

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+
+import { Product, HeroBanner, FooterBanner } from '../components';
 
 const Home = () => {
   return (
     <>
-      home
+      <HeroBanner />
+
+      <div className='products-heading'>
+        <h2>Best Selling Headphones</h2>
+        <p>Speakers of many variations</p>
+      </div>
+
+      <div className='products-container'>
+        {['product 1', 'products 2'].map((product) => product)}
+      </div>
+
+      <FooterBanner />  
     </>
   )
 }
