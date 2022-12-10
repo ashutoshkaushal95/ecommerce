893d9dcbd1e187103d73ef8e40025c3b5a27e7b9
ashutoshkaushal95 <ashutoshkaushal2@gmail.com>
2022-12-10 13:27:42 +0530

    layout completed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href="/">Audio Magic Store</Link>
+      </p>
+      <button type='button' className='cart-icon'>
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>3</span>
+      </button>
+    </div>
   )
 }
 
