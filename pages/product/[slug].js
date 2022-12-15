8d8ca558e5517fe0a25cbf48263062144cb03e21
ashutoshkaushal95 <ashutoshkaushal2@gmail.com>
2022-12-15 12:23:04 +0530

    context logic finished

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { client, urlFor } from '../../lib/client';
 import { Product } from '../../components';
+import { useStateContext } from '../../context/StateContext';
 
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product;
     const [index, setIndex] = useState(0);
+    
+    const { decQty, incQty, qty, onAdd } = useStateContext();
 
     return (
         <div>
@@ -42,13 +45,13 @@ const ProductDetails = ({ product, products }) => {
                     <div className="quantity">
                       <h3>Quantity: </h3>
                       <p className='quantity-desc'>
-                        <span className="minus" onClick="">
+                        <span className="minus" onClick={decQty}>
                           <AiOutlineMinus />
                         </span>
                         <span className="num" onClick="">
-                          0
+                          {qty}
                         </span>
-                        <span className="plus" onClick="">
+                        <span className="plus" onClick={incQty}>
                           <AiOutlinePlus />
                         </span>
 
@@ -58,7 +61,7 @@ const ProductDetails = ({ product, products }) => {
                       <button 
                           type='button' 
                           className='add-to-cart' 
-                          onClick="">
+                          onClick={() => onAdd(product, qty)}>
                           Add to Cart
                       </button>
                       <button 
